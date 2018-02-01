656a23d1cf0b1320eef2e8375ad5671cb3c4b4f5
Tyrant Davis <davistyrant@gmail.com>
2018-02-01 14:46:45 -0600

    Modify Square component

--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import './index.css';
 
 // reders a single button
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return(
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
