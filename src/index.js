8879869003c4c364b7729b4b441c36d497ca90bb
Tyrant Davis <davistyrant@gmail.com>
2018-02-02 09:50:25 -0600

    Update Board's render function

--- src/index.js
+++ src/index.js
@@ -43,7 +43,6 @@ class Board extends React.Component {
 
     return (
       <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
